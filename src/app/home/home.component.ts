9c900dcdec90dbb55b11eb5806eef4877c8fc099
file098 <filippodigennaro98@outlook.it>
2023-05-15 17:11:59 +0200

    added animations

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import {
+  AfterContentChecked,
+  AfterContentInit,
+  AfterViewInit,
+  Component,
+} from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {
+export class HomeComponent implements AfterContentInit {
   works: any = [
     { source: '../../assets/wall.jpg' },
     { source: '../../assets/wall.jpg' },
@@ -16,4 +21,21 @@ export class HomeComponent {
     { source: '../../assets/wall.jpg' },
     { source: '../../assets/wall.jpg' },
   ];
+
+  ngAfterContentInit(): void {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('show');
+          console.log(entry.target.classList);
+        } else {
+          entry.target.classList.remove('show');
+          console.log(entry.target.classList);
+        }
+      });
+    });
+
+    const hidden = document.querySelectorAll('.hidden');
+    hidden.forEach((el) => observer.observe(el));
+  }
 }
